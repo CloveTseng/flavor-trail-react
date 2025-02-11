2ba328cf406d081fd7038b2b016ee53ce2253cf3
Ariel <sonyt605@gmail.com>
2025-02-11 15:56:01 +0800

    搬運 pages/AllPost

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import { useState } from 'react'
-
+import AllPost from './allPost.jsx'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button className="btn btn-primary" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AllPost />
     </>
   )
 }
