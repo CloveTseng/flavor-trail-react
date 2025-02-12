54c15874d67bf5d0280066a6d2d9d3d825203ea6
DolceTseng <jessica.turn613@gmail.com>
2025-02-12 12:56:42 +0800

    add pages/Event

---
+++ layout/event/EventAbout.jsx
@@ -0,0 +1,47 @@
+function EventAbout() {
+    return (<>
+        <section className="event-about bg-primary py-12 py-lg-18">
+            <div className="container-lg">
+            <div className="row px-sm-4">
+                <h2 className="fs-4 fs-lg-3 fw-bold mb-5 mb-lg-7 text-white px-lg-0">
+                ABOUT EVENT
+                </h2>
+                <h3 className="event-about-title fw-bolder text-white mb-5 mb-lg-7 lh-1 px-lg-0">
+                關於街友送餐活動
+                </h3>
+                <p className="event-about-text text-primary-200 fw-medium fs-5 fs-lg-4 px-lg-0">
+                是不是感覺週末空虛寂寞想找點樂子？
+                <br />
+                來參加我們的「愛心分享：街友送餐活動」吧！
+                </p>
+            </div>
+            <div className="event-about-describe px-4 px-lg-0">
+                <div className="row">
+                <div className="col-lg-6 col-xxl-8">
+                    <svg className="event-about-deco mx-sm-4 mx-lg-0 mb-lg-14" width="56" height="48" viewBox="0 0 139 120"
+                    fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M138.197 84.1352C135.856 68.0918 125.181 58.9666 109.516 59.1754C103.844 52.1812 96.4868 35.645 112.433 4.17628C81.6077 26.0295 51.5451 119.705 112.433 115.885C127.256 114.955 140.537 100.179 138.197 84.1352Z"
+                        fill="#FF814A" />
+                    <path
+                        d="M60.2012 92.345C60.259 76.1308 51.0509 65.518 35.5285 63.3934C30.9537 55.6326 27.3676 28.7477 47.7907 0C14.075 17.0245 -30.7514 114.628 30.0278 119.91C44.8245 121.196 60.1434 108.559 60.2012 92.345Z"
+                        fill="#FF814A" />
+                    </svg>
+                    <p className="fs-4 fs-lg-2 text-white fw-medium d-none d-lg-block">
+                        你不僅能為生活在街頭的朋友們提供溫暖的食物，
+                        <br className="d-none d-lg-block" />
+                        還能與一群有著共同理念的新朋友們共度美好時光。
+                        <br />
+                        在這裡，每一份餐點不只是食物，更是滿載著愛心和希望。
+                        <br />
+                        讓我們攜手將這份愛傳遞給更多需要的人！
+                    </p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </section>
+    </>)
+}
+
+export default EventAbout
---
+++ layout/event/EventDeliverMeals.jsx
@@ -0,0 +1,31 @@
+function EventDeliverMeals()  {
+    return (<>
+        <section className="event-deliver-meals bg-primary py-18 py-lg-19 position-relative mb-19">
+            <div className="container">
+                <div className="row flex-column flex-lg-row gap-19 align-items-center justify-content-between">
+                    <div className="col-lg-4">
+                        <img className="event-deliver-meals-img img-fluid object-fit-cover" src="../assets/images/event-1.png"
+                        alt="event-picture1" />
+                    </div>
+                    <div className="col-lg-6">
+                        <img className="event-deliver-meals-img img-fluid object-fit-cover" src="../assets/images/event-2.png"
+                        alt="event-picture2" />
+                    </div>
+                </div>
+                <h2
+                className="event-deliver-meals-title col-9 col-sm-6 col-xl-12 text-lg-center text-nowrap text-white position-absolute">
+                    <div className="d-block d-lg-none">
+                        為街友<br />
+                        送餐，<br />
+                        傳遞愛心<br />
+                        與溫暖
+                    </div>
+                    <div className="d-none d-lg-block">
+                        為街友送餐，傳遞愛心與溫暖
+                    </div>
+                </h2>
+            </div>
+        </section>
+    </>)
+}
+export default EventDeliverMeals
