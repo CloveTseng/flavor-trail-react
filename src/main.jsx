80207f5c4d8bdf86c28654fafd545d2fa8edc644
Mingx1994x <mggxmingx@gmail.com>
2025-02-13 21:45:56 +0800

    chore: 修改routes資料夾名稱成小寫

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
-import router from "./Routes";
+import router from "./routes";
 import "../assets/scss/all.scss";
 // import App from './App.jsx'
 
