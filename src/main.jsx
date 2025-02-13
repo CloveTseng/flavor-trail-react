4baa5aaed289313c556f62155a4e3474dda057ed
Haojing XU <leslie.hj.xu.design@gmail.com>
2025-02-13 01:27:24 +0100

    refactor: convert about page to react

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { RouterProvider } from "react-router";
 import router from "./Routes";
 import "../assets/scss/all.scss";
 // import App from './App.jsx'
+import AboutUsPage from "./pages/AboutUsPage";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
     <RouterProvider router={router} />
+    <AboutUsPage />
   </StrictMode>
 );
