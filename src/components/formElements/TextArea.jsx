d13ff2dcd0409c34eb5a0ff81e0623b0a8648df3
DolceTseng <jessica.turn613@gmail.com>
2025-03-09 22:49:03 +0800

    update textarea

--- src/components/formElements/TextArea.jsx
+++ src/components/formElements/TextArea.jsx
@@ -6,6 +6,7 @@ const TextArea = ({ register, errors, labelText, id, rows, name }) => {
       rows={rows}
       placeholder={`請輸入${labelText}`}
       className="form-control py-2 px-5 border-gray-400 rounded-3 bg-white"
+      {...register(id)}
     ></textarea>
   );
 };
