eae0c6f2041608bbb2b1bbaa9334fa0780acd265
DolceTseng <jessica.turn613@gmail.com>
2025-03-10 17:58:19 +0800

    update accountSetting route

--- src/pages/account/AccountPosts.jsx
+++ src/pages/account/AccountPosts.jsx
@@ -3,6 +3,8 @@ import AccountFilter from '../../components/account/AccountFilter';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { Link } from 'react-router';
+import ShareFoodModal from '../../components/ShareFoodModal';
+import DeletePostModal from '../../components/account/DeletePostModal';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const ID = '1';
@@ -250,17 +252,29 @@ function AccountPosts() {
                           </a>
                           <ul className="dropdown-menu">
                             <li>
-                              <a className="dropdown-item" href="#">
+                              <a
+                                className="dropdown-item"
+                                href="#"
+                                data-bs-toggle="modal"
+                                data-bs-target="#shareFoodModal"
+                              >
                                 編輯貼文
                               </a>
                             </li>
                             <li>
-                              <a className="dropdown-item" href="#">
+                              <a
+                                className="dropdown-item"
+                                href="#"
+                                data-bs-toggle="modal"
+                                data-bs-target="#deletePostModal"
+                              >
                                 刪除貼文
                               </a>
                             </li>
                           </ul>
                         </div>
+                        <ShareFoodModal />
+                        <DeletePostModal />
                         {/* dropdown */}
                         <div className="post-card-img my-7 text-center">
                           <Link
